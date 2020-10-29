35c5e5cfb2fc4cb6334576692ce7ddfda634a2d0
srivastavamonika <71062573+srivastavamonika@users.noreply.github.com>
2020-10-29 23:17:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function (window) {
-    var names = ["Adam", "Jonny", "Henna", "Jsssica", "Parul", "James", "Lavi", "Piya", "Maina", "Nia"];
+    var names = ["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
     
 for (var i = 0; i < names.length; i++) {
 var firstLetter = names[i].charAt(0).toLowerCase(); 
